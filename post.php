2de9302f495812d68ecb7adbc9a4f3bb3069f8d7
PrakashGitDev <91112295+PrakashGitDev@users.noreply.github.com>
2021-10-09 15:20:38 +0545

    updated

--- post.php
+++ post.php
@@ -43,9 +43,9 @@
     <!-- header section ends -->
     <!-- posts section starts  -->
 
-    <section class="container spacepost" id="posts">
+    <section class="container top" id="posts">
 
-        <div class="posts-container nested">
+        <div class="posts-container">
                 <div class="post">
                     <img src="images/blog-1.jpg" alt="" class="postimage">
                     <div class="date">
@@ -86,10 +86,76 @@
                     <span>(29)</span>
                 </a>
             </div>
+                </div>
+                <h2 class="releted">Releted Posts</h2>
+            <div class="posts-container">
+            <a href="post.php">
+                <div class="post">
+                    <img src="images/blog-1.jpg" alt="" class="image">
+                    <div class="date">
+                        <i class="far fa-clock"></i>
+                        <span>1st may, 2021</span>
+                    </div>
+                    <h3 class="title">blog title goes here</h3>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias rerum
+                        numquam,
+                        quos aut est culpa quisquam excepturi sed a inventore dicta tempore consequuntur possimus magnam
+
+                    </p>
+                
+            </a>
+
+            <div class="links">
+                <a href="#" class="user">
+                    <i class="far fa-user"></i>
+                    <span>Admin</span>
+                </a>
+                <a href="#" class="icon">
+                    <i class="far fa-comment"></i>
+                    <span>(45)</span>
+                </a>
+                <a href="#" class="icon">
+                    <i class="far fa-share-square"></i>
+                    <span>(29)</span>
+                </a>
+            </div>
         </div>
 
+            <a href="#">
+                <div class="post">
+                    <img src="images/blog-2.jpg" alt="" class="image">
+                    <div class="date">
+                        <i class="far fa-clock"></i>
+                        <span>1st may, 2021</span>
+                    </div>
+                    <h3 class="title">blog title goes here</h3>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias rerum
+                        numquam,
+                        quos aut est culpa quisquam excepturi sed a inventore dicta tempore consequuntur possimus magnam
+                    </p>
+                
+            </a>
+            <div class="links">
+                <a href="#" class="user">
+                    <i class="far fa-user"></i>
+                    <span>Admin</span>
+                </a>
+                <a href="#" class="icon">
+                    <i class="far fa-comment"></i>
+                    <span>(45)</span>
+                </a>
+                <a href="#" class="icon">
+                    <i class="far fa-share-square"></i>
+                    <span>(29)</span>
+                </a>
+            </div>
+        </div>
 
         </div>
+        </div>
+        
+
+       
 
 
 
