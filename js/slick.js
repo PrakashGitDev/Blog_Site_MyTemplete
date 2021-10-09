fa18a03751499954db512349a420bc8c7db2c718
PrakashGitDev <91112295+PrakashGitDev@users.noreply.github.com>
2021-10-09 12:36:31 +0545

    Initial commit
    Updated

---
+++ js/slick.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){      
+    $('.slider_inner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        });
+})
