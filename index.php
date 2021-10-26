3a8bfc8fa81529ffe2e9fc59d3ac1395ad5ac52e
PrakashGitDev <91112295+PrakashGitDev@users.noreply.github.com>
2021-10-26 11:16:36 +0545

    updated

--- index.php
+++ index.php
@@ -88,12 +88,141 @@
                     </div>
             </div>
 
+ 
+
+            <a href="post.php">
+            <div class="post">
+                    <img src="images/blog-1.jpg" alt="" class="image">
+                    <div class="date">
+                        <i class="far fa-clock"></i>
+                        <span>1st may, 2021</span>
+                    </div>
+                    <h3 class="title">blog title goes here</h3>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias rerum
+                        numquam,
+                        quos aut est culpa quisquam excepturi sed a inventore dicta tempore consequuntur possimus magnam
+
+                    </p>
+                
+        </a>
+
+                    <div class="links">
+                        <a href="#" class="user">
+                            <i class="far fa-user"></i>
+                            <span>Admin</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-comment"></i>
+                            <span>(45)</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-share-square"></i>
+                            <span>(29)</span>
+                        </a>
+                    </div>
+            </div>
+            <a href="post.php">
+            <div class="post">
+                    <img src="images/blog-1.jpg" alt="" class="image">
+                    <div class="date">
+                        <i class="far fa-clock"></i>
+                        <span>1st may, 2021</span>
+                    </div>
+                    <h3 class="title">blog title goes here</h3>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias rerum
+                        numquam,
+                        quos aut est culpa quisquam excepturi sed a inventore dicta tempore consequuntur possimus magnam
+
+                    </p>
+                
+        </a>
+
+                    <div class="links">
+                        <a href="#" class="user">
+                            <i class="far fa-user"></i>
+                            <span>Admin</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-comment"></i>
+                            <span>(45)</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-share-square"></i>
+                            <span>(29)</span>
+                        </a>
+                    </div>
+            </div>
+            <a href="post.php">
+            <div class="post">
+                    <img src="images/blog-1.jpg" alt="" class="image">
+                    <div class="date">
+                        <i class="far fa-clock"></i>
+                        <span>1st may, 2021</span>
+                    </div>
+                    <h3 class="title">blog title goes here</h3>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias rerum
+                        numquam,
+                        quos aut est culpa quisquam excepturi sed a inventore dicta tempore consequuntur possimus magnam
+
+                    </p>
+                
+        </a>
+
+                    <div class="links">
+                        <a href="#" class="user">
+                            <i class="far fa-user"></i>
+                            <span>Admin</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-comment"></i>
+                            <span>(45)</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-share-square"></i>
+                            <span>(29)</span>
+                        </a>
+                    </div>
+            </div>
+            <a href="post.php">
+            <div class="post">
+                    <img src="images/blog-1.jpg" alt="" class="image">
+                    <div class="date">
+                        <i class="far fa-clock"></i>
+                        <span>1st may, 2021</span>
+                    </div>
+                    <h3 class="title">blog title goes here</h3>
+                    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias rerum
+                        numquam,
+                        quos aut est culpa quisquam excepturi sed a inventore dicta tempore consequuntur possimus magnam
+
+                    </p>
+                
+        </a>
+
+                    <div class="links">
+                        <a href="#" class="user">
+                            <i class="far fa-user"></i>
+                            <span>Admin</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-comment"></i>
+                            <span>(45)</span>
+                        </a>
+                        <a href="#" class="icon">
+                            <i class="far fa-share-square"></i>
+                            <span>(29)</span>
+                        </a>
+                    </div>
+            </div>
+            
 
 
 
+                    </div>
+                    </div>
             <?php include 'includes/sidebar.php';?>
 
-        </div>
+        
         
 
 
