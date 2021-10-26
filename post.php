e317a3833a23e71ad9697ff7eef497f7a26fb499
PrakashGitDev <91112295+PrakashGitDev@users.noreply.github.com>
2021-10-26 11:17:24 +0545

    Updated

--- post.php
+++ post.php
@@ -232,28 +232,6 @@
 
     <!-- posts section ends -->
 
-    <!-- contact section starts  -->
-
-    <section class="contact" id="contact">
-
-        <form action="">
-            <h3>Contact Me</h3>
-            <div class="inputBox">
-                <input type="text" placeholder="name">
-                <input type="email" placeholder="email">
-            </div>
-            <div class="inputBox">
-                <input type="number" placeholder="number">
-                <input type="text" placeholder="subject">
-            </div>
-            <textarea name="" placeholder="message" id="" cols="30" rows="10"></textarea>
-            <input type="submit" value="send message" class="btn">
-        </form>
-
-    </section>
-
-    <!-- contact section ends -->
-
     <!-- footer section starts  -->
 
     <section class="footer">
